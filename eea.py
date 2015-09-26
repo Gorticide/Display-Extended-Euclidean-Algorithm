608d3074f936407ffbb6d7f3fefa6b3aa333c5a8
Gorticide <missionmike1967@yahoo.com>
2015-09-26 16:05:55 -0400

    Euclidean Recipe

--- eea.py
+++ eea.py
@@ -1,9 +1,24 @@
 # Michael Hentrich is altering this script so as to create pedagical
 # version which will display each step of the process.
+# Date: September 2015
 # euclid(a,b)
 # ext_euclid(a,b)
 # xed(a,b) for displaying steps of the process
-# Date: 9/22/2015
+# xlgcd(a,b) - like xed but includes detailed summary
+# isolve(a,b,d) - solves Diophntine equations using recersive euclid
+# iterative_egcd(a, b)
+# modinv(a, m)
+# icrt(a, n) - Chinese Remainder Theorem
+# mul_inv(a, b) - multiplicative modular inverse
+# 
+# Kirby Urner for a cooler version of the Extended Euclidean Algorithm, with
+# related inverse and Chinese Remainder Theorem functions.
+#
+# I alter his code slightly by using:
+# from numpy import product, subtract, add
+# Functions used from number_theory.py:  eea, inv, crt, gcd, xgcd, lcm, cong
+#
+#
 #
 # Author: Sam Erickson for test(a,b) and getCoeffic(a,b)
 # Date: 7/14/2013
@@ -279,3 +294,272 @@ def xlgcd(a, b):
     else:
         print "There's been some kind of error."
     raw_input("Press enter")
+
+def isolve(a,b,d):
+    q, r = divmod(a, b)
+    if r == 0:
+        return ([0, d/b])
+    else:
+        sol = isolve(b,r,d)
+        u = sol[0]
+        v = sol[1]
+        return ([v, u - q*v])
+    
+# Iterative Algorithm (xgcd)
+def iterative_egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q,r = b//a,b%a; m,n = x-u*q,y-v*q # use x//y for floor "floor division"
+        b,a, x,y, u,v = a,r, u,v, m,n
+    return b, x, y
+
+def modinv(a, m):
+    g, x, y = iterative_egcd(a, m) 
+    if g != 1:
+        raise ZeroDivisionError, "Inverse does not exist."
+    else:
+        return x % m
+
+
+
+# Chinese Remainder Theorem
+# USAGE: icrt(a[], n[])
+# Following the form of Sage:
+# first parameter: list of a_i, second parameterL list of modulo n_i
+
+def icrt(a, n):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+ 
+    for a_i, n_i in zip(a, n):
+        p = prod / n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+ 
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while (a > 1):
+        if b == 0: return 0
+        q = a / b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+
+# The MotherLoad for Number Theory: see Brian Gladman
+
+def gcd(a, *r):
+  '''
+  Greatest Common Divisor of a sequence of values
+
+  >>> gcd(1701, 13979)
+  7
+
+  >>> gcd(117, -17883411)
+  39
+
+  >>> gcd(3549, 70161,  336882, 702702)
+  273
+  '''
+  for b in r:
+    while b:
+      a, b = b, a % b
+  return abs(a)
+
+
+def lcm(a, *r):
+  '''
+  Least Common Multiple Divisor of a sequence of values
+
+  >>> lcm(1701, 13979)
+  3396897
+
+  >>> lcm(117, -17883411)
+  53650233
+
+  >>> lcm(3549, 70161,  336882, 702702)
+  111426753438
+  '''
+  for b in r:
+    a *= b // gcd(a, b)
+  return abs(a)
+
+def xgcd(a, b):
+  '''
+  Euclid's Extended GCD Algorithm
+
+  >>> xgcd(314159265, 271828186)
+  (-18013273, 20818432, 7)
+  '''
+  u, u1 = 1, 0
+  v, v1 = 0, 1
+  while b:
+    q, r = divmod(a,  b)
+    a, b = b, r
+    u, u1 = u1, u - q * u1
+    v, v1 = v1, v - q * v1
+  return (u, v, a) if a > 0 else (-u, -v, -a)
+
+def cong(a, p, n):
+  '''
+  Solve the congruence a * x == p (mod n)
+
+  >>> cong(13, 17, 19)
+  13
+
+  >>> cong(17, 19, 23)
+  16
+
+  >>> cong(14, 6, 91) is None
+  True
+  '''
+  g = gcd(a, n)
+  if p % g > 0:
+    return None
+  return (xgcd(a, n)[0] * (p // g)) % (n // g)
+
+def inv(a, n):
+  '''
+  Find the modular inverse of a (mod n)
+
+  >>> inv(271828, 314159)
+  898
+
+  >>> inv(314159, 271828)
+  271051
+  '''
+  return None if gcd(a, n) > 1 else xgcd(a, n)[0] % n
+
+
+def crt(a, m):
+  '''
+  The Chinese Remainder Theorem (CRT)
+
+  Solve the equations x = a[i] mod m[i] for x
+
+  >>> crt((2, 3, 5, 7), (97, 101, 103, 107))
+  96747802
+  '''
+  def _crt(a, b, m, n):
+    d = gcd(m, n)
+    if (a - b) % d:
+      return None
+    x, y, z = m // d, n // d, (m * n) // d
+    p, q, r = xgcd(x, y)
+    return (b * p * x + a * q * y) % z
+
+  if len(a) == len(m):
+    x, mm = a[0], m[0]
+    for i in range(1, len(a)):
+      x = _crt(a[i], x, m[i], mm)
+      if x is None:
+        break
+      mm = lcm(m[i], mm)
+  else:
+    raise IndexError
+  return x
+
+# Curiosities
+# These I imported functions from numpy to make work
+# They work right, but I want to research map, reduce, sum, subtract, etc
+
+from numpy import product, subtract, add
+from operator import mod
+
+def eea_map(a,b):
+    """Extended Euclidean Algorithm for GCD
+
+    This EEA returns a 3-tuple, the 0th element being the gcd(a,b),
+    the next two being s,t such that gcd(a,b) = s*a + t*b, i.e.
+    two integers which bring the initial a,b within the gcd of
+    one another.
+
+    Example:
+
+    >>> eea_map(17,25)            # gcd(a,b) = 1
+    [1, 3, -2]
+    >>> 3*17 - 2*25           # s = 3, t = -2
+    1
+
+    >>> eea_map(29834,8282)       # gcd(a,b) = 2
+    [2, -88, 317]
+    >>> -88*29834 + 317*8282  # s = -88, t = 317
+    2
+
+    With EEA, you can also find the inverse of x mod n, provided
+    gcd(x,n)=1.  The inverse is that number which, multiplying
+    x, gives a remainder of 1 when divided by n.
+
+    Example:  find x such that (x * 7) mod 4 = 1.
+
+    Computing:
+
+    >>> inverse(7,4)
+    3
+
+    i.e. (3*7) mod 4 = 21 mod 4 = integer remainder of 21/4 = 1.
+    """
+    v1 = [a,1,0]
+    v2 = [b,0,1]
+    while v2[0]<>0:
+       p = v1[0]//v2[0] # floor division
+       v2, v1 = map(subtract,v1,[p*vi for vi in v2]), v2
+    return v1
+
+def inverse(m,k):
+     """
+     Return b such that b*m mod k = 1, or 0 if no solution
+     """
+     v = eea_map(m,k)
+     return (v[0]==1)*(v[1] % k)
+
+def xcrt(a, m):
+     """
+     Chinese Remainder Theorem:
+     m = list of pairwise relatively prime integers
+     a = remainders when x is divided by m
+     (ai is 'each in a', mi 'each in m')
+
+     The solution for x modulo M (M = product of m) will be:
+     x = a1*M1*y1 + a2*M2*y2 + ... + ar*Mr*yr (mod M),
+     where Mi = M/mi and yi = (Mi)^-1 (mod mi) for 1 <= i <= r.
+
+     The Chinese Remainder Theorem states that if I give you
+     a smattering of divisors, all coprime to each other, and
+     tell you what the remainders for each of them is, then
+     you can tell me a number which meets my specifications.
+
+     Example:  Your divisors are 7,11 and 15.  The respective
+     remainders are 2, 3 and 0.  What's a number that works?
+
+     Computing:
+
+     >>> xcrt([2,3,0], [7,11,15])
+     135
+
+     Check:
+
+     >>> 135 % 7
+     2
+     >>> 135 % 11
+     3
+     >>> 135 % 15
+     0 
+
+     Works.
+     """
+
+     M  = product(m)        # multiply m together
+     Ms = [M/mi for mi in m]   # list of all M/mi
+     ys = [inverse(Mi, mi) for Mi,mi in zip(Ms,m)] # uses inverse,eea
+     return sum([ai*Mi*yi for ai,Mi,yi in zip(a,Ms,ys)]) % M
+
+# def prod( iterable ):
+#    p = 1
+#    for n in iterable:
+#        p *= n
+#        return p
